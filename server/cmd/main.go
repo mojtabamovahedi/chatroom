5f2b2c7271cf033ddd61a90ec716cf9ccf35fd22
mojtabamovahedi <mojtabamovahedi.eng@gmail.com>
2025-01-24 09:58:24 +0330

    add graceful shutdown

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -5,6 +5,9 @@ import (
 	"github.com/mojtabamovahedi/chatroom/server/app"
 	"github.com/mojtabamovahedi/chatroom/server/config"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -12,5 +15,24 @@ func main() {
 	cfg := config.MustReadConfig("config.json")
 	appContainer := app.MustNewApp(cfg)
 
-	log.Fatal(http.Run(appContainer, cfg.Server))
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	server := make(chan error, 1)
+
+	go func() {
+		server <- http.Run(appContainer, cfg.Server)
+	}()
+
+	select {
+	case <-quit:
+		log.Println("shutting down...")
+	case err := <-server:
+		if err != nil {
+			log.Fatalf("Server error: %s", err)
+		}
+	}
+
+	appContainer.Shutdown()
+	log.Println("Server stopped")
 }
